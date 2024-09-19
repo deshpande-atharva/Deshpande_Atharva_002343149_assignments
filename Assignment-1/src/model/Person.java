3dd04ee99e4b8c5981ec9ff2c55f2bd6a4829e3f
deshpande-atharva <deshpande.atha@northeastern.edu>
2024-09-19 14:34:31 -0400

    Assignment-1

---
+++ Assignment-1/src/model/Person.java
@@ -0,0 +1,194 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package model;
+
+/**
+ *
+ * @author athar
+ */
+public class Person {
+    String firstName;
+    String lastname;
+    String email;
+    String phone;
+    String licenseNumber;
+    String licenseState;
+    String ssn;
+    String addressLine1;
+    String addressLine2;
+    String city;
+    String state;
+    String zip;
+    String telenumber;
+    String faxNumber;
+    String gender;
+    String nationality;
+    String mothertonge;
+    String highestdegree;
+    String dateOfBrith;
+    String major;
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getLicenseNumber() {
+        return licenseNumber;
+    }
+
+    public void setLicenseNumber(String licenseNumber) {
+        this.licenseNumber = licenseNumber;
+    }
+
+    public String getLicenseState() {
+        return licenseState;
+    }
+
+    public void setLicenseState(String licenseState) {
+        this.licenseState = licenseState;
+    }
+
+    public String getSsn() {
+        return ssn;
+    }
+
+    public void setSsn(String ssn) {
+        this.ssn = ssn;
+    }
+
+    public String getAddressLine1() {
+        return addressLine1;
+    }
+
+    public void setAddressLine1(String addressLine1) {
+        this.addressLine1 = addressLine1;
+    }
+
+    public String getAddressLine2() {
+        return addressLine2;
+    }
+
+    public void setAddressLine2(String addressLine2) {
+        this.addressLine2 = addressLine2;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    public String getZip() {
+        return zip;
+    }
+
+    public void setZip(String zip) {
+        this.zip = zip;
+    }
+
+    public String getTelenumber() {
+        return telenumber;
+    }
+
+    public void setTelenumber(String teleumber) {
+        this.telenumber = teleumber;
+    }
+
+    public String getFaxNumber() {
+        return faxNumber;
+    }
+
+    public void setFaxNumber(String faxNumber) {
+        this.faxNumber = faxNumber;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public String getNationality() {
+        return nationality;
+    }
+
+    public void setNationality(String nationality) {
+        this.nationality = nationality;
+    }
+
+    public String getMothertonge() {
+        return mothertonge;
+    }
+
+    public void setMothertonge(String mothertonge) {
+        this.mothertonge = mothertonge;
+    }
+
+    public String getHighestdegree() {
+        return highestdegree;
+    }
+
+    public void setHighestdegree(String highestdegree) {
+        this.highestdegree = highestdegree;
+    }
+
+    public String getDateOfBrith() {
+        return dateOfBrith;
+    }
+
+    public void setDateOfBrith(String dateOfBrith) {
+        this.dateOfBrith = dateOfBrith;
+    }
+
+    public String getMajor() {
+        return major;
+    }
+
+    public void setMajor(String major) {
+        this.major = major;
+    }
+    
+    
+}
